b348c18203a6ecf96abc59e9047cf9b310448e71
Ben N <bentinata@gmail.com>
2018-07-30 12:21:55 +0700

    Pull current branch instead of hardcoded origin master.

--- assets/webhook/deploy.sh
+++ assets/webhook/deploy.sh
@@ -11,7 +11,7 @@ R=$(git rev-parse @{u})
 
 if [ $L != $R ]; then
   git status -uno
-  git pull origin master --force > $log
+  git pull --force > $log
   composer install >> $log
   mysql db < db/schema.sql
   mysql db < db/data.sql
