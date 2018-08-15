ba179082b1a76ca52577d0e37418bb30ff339288
Ben N <bentinata@gmail.com>
2018-08-15 16:56:25 +0700

    Remove db dump. It's only for developers.

--- assets/webhook/deploy.sh
+++ assets/webhook/deploy.sh
@@ -14,8 +14,6 @@ if [ $L != $R ]; then
   git reset --hard HEAD >> $log
   git pull --force >> $log
   composer install >> $log
-  mysql db < db/schema.sql
-  mysql db < db/data.sql
   curl -g "https://api.telegram.org/${token}/sendMessage?chat_id=${chat}&parse_mode=markdown&text=[Site](${site}) updated. [Deploy Log](${site}:9000/hooks/deploy). [Current Status](${site}:9000/hooks/status).&"
 fi
 
